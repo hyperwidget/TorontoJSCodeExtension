4693ad95e72da119e60f6a244e19e7d908547e1c
Hunter Jansen <rawkamatic@gmail.com>
2019-11-24 10:52:17 -0500

    step2

--- src/extension.ts
+++ src/extension.ts
@@ -12,15 +12,12 @@ export function activate(context: vscode.ExtensionContext) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
-    "extension.helloWorld",
-    () => {
-      // The code you place here will be executed every time your command is executed
+  let disposable = vscode.commands.registerCommand("extension.hellotjs", () => {
+    // The code you place here will be executed every time your command is executed
 
-      // Display a message box to the user
-      vscode.window.showInformationMessage("Hello World!");
-    }
-  );
+    // Display a message box to the user
+    vscode.window.showInformationMessage("Hello TJS!");
+  });
 
   context.subscriptions.push(disposable);
 }
